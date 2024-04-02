dd291465480bbfa2c17975b0b313abff7dece248
Dana Contreras <danabcontreras@gmail.com>
2024-04-02 20:26:29 -0300

    feat(splash): update splash screen and icon with CliMarte logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaView, StatusBar, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import Menu from './navigation/Menu/Menu';
+import SplashScreen from 'react-native-splash-screen'
 
 const App = () => {
+
+  useEffect(() => {
+    SplashScreen.hide()
+  }, [])
+
   return (
     <View style={{flex: 1}}>
-      <StatusBar backgroundColor="transparent" translucent={true} barStyle='light-content' />
       <NavigationContainer>
         <Menu/>
       </NavigationContainer>
